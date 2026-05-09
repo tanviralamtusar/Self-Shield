226e14149d97e19ce87eafceba6b03bc54ae30a1
mdmehrabahmedsaif <mdmehrabahmedsaif@gmail.com>
2026-05-09 13:56:51 +0600

    fix: move accessibility service to app manifest and remove Hilt annotation for proper visibility

--- self-shield-android/service/accessibility/src/main/java/com/selfshield/service/accessibility/SelfShieldAccessibilityService.kt
+++ self-shield-android/service/accessibility/src/main/java/com/selfshield/service/accessibility/SelfShieldAccessibilityService.kt
@@ -3,9 +3,7 @@ package com.selfshield.service.accessibility
 import android.accessibilityservice.AccessibilityService
 import android.view.accessibility.AccessibilityEvent
 import android.view.accessibility.AccessibilityNodeInfo
-import dagger.hilt.android.AndroidEntryPoint
 
-@AndroidEntryPoint
 class SelfShieldAccessibilityService : AccessibilityService() {
 
     override fun onAccessibilityEvent(event: AccessibilityEvent) {
