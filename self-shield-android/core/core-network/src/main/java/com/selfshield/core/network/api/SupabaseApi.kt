a172e89e327866f63fceb99d1d7c9f90d9c48ada
tanviralamtusar <77721756+tanviralamtusar@users.noreply.github.com>
2026-05-09 18:01:29 +0600

    feat: add SupabaseApi interface for device registration and claiming endpoints

--- self-shield-android/core/core-network/src/main/java/com/selfshield/core/network/api/SupabaseApi.kt
+++ self-shield-android/core/core-network/src/main/java/com/selfshield/core/network/api/SupabaseApi.kt
@@ -1,5 +1,7 @@
 package com.selfshield.core.network.api
 
+import com.selfshield.core.network.model.ClaimRequest
+import com.selfshield.core.network.model.ClaimResponse
 import com.selfshield.core.network.model.RegisterRequest
 import com.selfshield.core.network.model.RegisterResponse
 import retrofit2.Response
